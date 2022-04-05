e0a7ce569e9b58633c992d96d4a2aa2f344ed5f0
chrislozzi <mounede7@gmail.com>
2022-04-05 14:37:23 +0200

    exercice1.2

---
+++ src/FactorielExepction.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+//Exercice 1.2 gestion d'une erreur de saisie, je n'ai pas reussis à gérer le java.lang.StackOverflowError
+public class FactorielExepction {
+	public static long fact(long n) {
+		
+		if( n <= 0) {
+			return 1;
+			}
+		else {
+			return n*fact(n -1);//appel récursif
+		}
+	}
+
+	public static void main(String[] args) {
+		Long n = 0L;
+		System.out.println("Entrez un entier pour calculer n!");
+		try {
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextLong();
+		}
+		catch( java.util.InputMismatchException e) {
+			System.out.println("exception int attendue : " + e);
+		}
+		System.out.println( n +"! = " + fact(n));
+	}
+
+}
--- src/FoundException.java
+++ src/FoundException.java
@@ -9,11 +9,11 @@ public class FoundException {
 		
 		
 		//Exercice 1.1
+		//date.getClass() leve l'exception : java.lang.NullPointerException, nous devons la capturer 
+		//afin de permettre à l'instruction suivant de s'executer
 		try {
-		
-		System.out.println(today.getClass().getName());
+		//
 		System.out.println(date.getClass().getName());
-		
 		}
 		catch(java.lang.NullPointerException e) {
 			System.out.println("exception capturée :" + e);
